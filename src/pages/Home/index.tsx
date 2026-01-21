778f86730dff83e6fc89d22ac860f91d47b81797
thiagodnt <thiagodnt@hotmail.com>
2026-01-20 21:21:54 -0300

    feat: estilização inicial home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-	return <div>Página HOME</div>;
+	return (
+		<div className="flex flex-col w-full py-4 items-center justify-center">
+			<h1 className="text-3xl md:text-4xl font-bold text-white mt-20">
+				Th14g0d
+			</h1>
+			<span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+			<main className="flex flex-col w-11/12 max-w-xl text-center">
+				<section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+					<a>
+						<p className="text-base md:text-lg">Canal no Youtube</p>
+					</a>
+				</section>
+				<footer className="flex justify-center gap-3 my-4"></footer>
+			</main>
+		</div>
+	);
 }
