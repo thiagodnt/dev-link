8d206ae753fc6d27b9c6c83987c5ccdc49d3a49b
thiagodnt <thiagodnt@hotmail.com>
2026-01-23 19:07:27 -0300

    chore: setup firebase auth e firestore

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import { useState, type FormEvent } from 'react';
 import { Link } from 'react-router';
 import Input from '../../components/Input';
+import { auth } from '../../services/firebaseConnection';
 
 export default function Login() {
 	const [email, setEmail] = useState<string>('');
