fd8c8d53689c2ff1669bd82d412324986578883b
thiagodnt <thiagodnt@hotmail.com>
2026-01-30 22:32:37 -0300

    feat: adicionar icon para o input nome do link e ajustar layout icon + label

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,7 +1,7 @@
 import { type FormEvent, useState } from 'react';
 import Header from '../../components/Header';
 import Input from '../../components/Input';
-import { FaLink } from 'react-icons/fa';
+import { FaFileSignature, FaLink } from 'react-icons/fa';
 import { FiTrash } from 'react-icons/fi';
 
 import { db } from '../../services/firebaseConnection';
@@ -54,7 +54,10 @@ export default function Admin() {
 				className="w-full max-w-xl flex flex-col mt-8 mb-4"
 				onSubmit={handleSubmit}
 			>
-				<label className="text-white font-medium my-2">Nome do link</label>
+				<label className="text-white font-medium my-2 flex items-center gap-1">
+					<FaFileSignature size={16} color="#FFF" />
+					Nome do link
+				</label>
 				<Input
 					type="text"
 					placeholder="Digite o nome do link..."
@@ -62,8 +65,8 @@ export default function Admin() {
 					onChange={(e) => setLinkNameInput(e.target.value)}
 				/>
 				<label className="text-white font-medium my-2 flex items-center gap-1">
-					URL
 					<FaLink size={16} color="#FFF" />
+					URL
 				</label>
 				<Input
 					type="url"
